cc8e2067a5c166ca548c31cd4c355f9468bd1280
hasimossimo <hasimossimo1989@gmail.com>
2019-11-09 22:41:58 +0900

    Add Chapter11

---
+++ Part1/Chapter11/MoneyTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Part1\Chapter11;
+
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testMultiplication()
+    {
+        $five = Money::dollar(5);
+        $this->assertTrue(Money::dollar(10)->equals($five->times(2)));
+        $this->assertTrue(Money::dollar(15)->equals($five->times(3)));
+    }
+
+    /**
+     *
+     */
+    public function testEquality()
+    {
+        $five = Money::dollar(5);
+        $this->assertTrue($five->equals(Money::dollar(5)));
+        $this->assertFalse($five->equals(Money::dollar(6)));
+
+        $dollar = Money::dollar(5);
+        $franc = Money::franc(5);
+        $this->assertFalse($dollar->equals($franc));
+    }
+
+    /**
+     *
+     */
+    public function testCurrency()
+    {
+        $this->assertEquals('USD', Money::dollar(1)->currency());
+        $this->assertEquals('CHF', Money::franc(1)->currency());
+    }
+}
